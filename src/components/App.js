e39023fa5db41eba2b098ffaf8bb02b0212179c0
biu425 <864722075@qq.com>
2022-06-13 16:29:53 -0700

    ready for deploy

---
+++ src/components/App.js
@@ -0,0 +1,37 @@
+import React, { useEffect, useState } from 'react';
+import SearchBar from './searchBar';
+import VideoList from './videoList';
+import VideoDetail from './videoDetail';
+import useVideos from '../hooks/useVideos';
+
+const App = () => {
+    const [selectedVideo, setSelectedVideo] = useState(null);
+    const [videos, search] = useVideos('cats');
+
+    useEffect(() => {
+        setSelectedVideo(videos[0]);
+    }, [videos])
+    
+        return (
+            <div className='ui container'>
+                <SearchBar onFormSubmit={search} />
+                <div className='ui grid'>
+                    <div className='ui row'>
+                        <div className='eleven wide column' >
+                            <VideoDetail video={selectedVideo} />
+                        </div>
+                        <div className='five wide column'>
+                            <VideoList 
+                                onVideoSelect={setSelectedVideo} 
+                                videos={videos}
+                            />
+                        </div>
+                    </div>
+                </div>
+            </div>
+        );
+    };  
+
+
+
+export default App;
